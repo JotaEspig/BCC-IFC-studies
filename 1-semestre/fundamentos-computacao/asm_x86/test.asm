7baf772349856502cd35beaa77be40ae76327304
João Vitor Espig <jotaespig@gmail.com>
2023-03-29 19:17:50 -0300

    added subtraction

--- 1-semestre/fundamentos-computacao/asm_x86/test.asm
+++ 1-semestre/fundamentos-computacao/asm_x86/test.asm
@@ -2,6 +2,9 @@ section .data
     msg db "Hello World", 0xA
     msg_len equ $-msg
 
+section .bss
+    sub_val resb 4
+
 section .text
     global _start
 
@@ -9,9 +12,22 @@ _start:
     mov edx, msg_len
     mov ecx, msg
     mov ebx, 0x1
-    mov eax, 4
+    mov eax, 0x4
     int 0x80
 
-    mov ebx, 0
-    mov eax, 1
+    mov eax, 0xA
+    mov ebx, 0x6
+    not ebx
+    inc ebx
+    add eax, ebx
+    mov [sub_val], eax 
+
+    mov edx, 0x2
+    mov ecx, [sub_val]
+    mov ebx, 0x1
+    mov eax, 0x4
+    int 0x80      ; for some reason it doesnt show in the terminal the value 6
+
+    mov ebx, 0x0
+    mov eax, 0x1
     int 0x80
