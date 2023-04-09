2e9d462cd092b619eaff0d7b6623949d4362c6fb
João Vitor Espig <jotinha1300@gmail.com>
2023-04-09 01:09:40 -0300

    improved run script

--- other/os-test/run.sh
+++ other/os-test/run.sh
@@ -1,5 +1,11 @@
+filename=boot.asm
+
+if [ $# -eq 1 ]; then
+	filename=$1
+fi
+
 echo -e "\033[0;36massemblying\033[0m"
-nasm -f bin boot.asm -o out.bin
+nasm -f bin $filename -o out.bin
 qemu-system-x86_64 out.bin
 echo -e "\033[1;31mremoving out.bin\033[0m"
 rm out.bin
