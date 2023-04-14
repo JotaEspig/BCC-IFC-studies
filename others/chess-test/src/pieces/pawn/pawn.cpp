dc562561662d26ce71d560d694e5f93be2af9c96
João Vitor Espig <jotinha1300@gmail.com>
2023-04-13 23:50:32 -0300

    improved rules to check if move is possible

--- others/chess-test/src/pieces/pawn/pawn.cpp
+++ others/chess-test/src/pieces/pawn/pawn.cpp
@@ -1,5 +1,6 @@
 #include "pawn.hpp"
 
+#include <cstdlib>
 #include <cstdint>
 
 #include "../piece.hpp"
@@ -17,23 +18,27 @@ Pawn::~Pawn()
 uint8_t Pawn::isMovePossible(uint8_t file, uint8_t rank,
                              uint8_t fileTarget, uint8_t rankTarget)
 {
-    if (!Piece::isMovePossible(rank, file, rankTarget, fileTarget))
-        return false;
+    if (!Piece::isMovePossible(file, rank, fileTarget, rankTarget))
+        return 0;
 
-    // The code between line 24 and 29 checks if the pawn movement is correct
-    // according to its color. The decider variables means that if the pawn is
-    // white, it should increment the rank value, if it's black, it should
-    // decrement.
-    // e.g. 1.e4 - White pawn go from 2nd rank for 4th rank, so its
-    // rank value increased: So, (4 - 2) * 1 > 0
-    // And if it was 1... e5: go from 7th rank to
-    // 5th rank, so it decremented. So, (5 - 7) * -1 > 0 as well.
-    // If the calculation results in a value < 0 it's wrong, so it return false
+    uint8_t fileDiff = abs(fileTarget - file);
+    uint8_t rankDiff = abs(rankTarget - rank);
+    // decider defines whether pawn should go
+    // rankTarget > rank if white (*1)
+    // rankTarget < rank if black (*-1)
+    // if calculation is < 0, something is off
     int8_t decider = isWhite ? 1 : -1;
     if ((rankTarget - rank) * decider < 0)
-    {
-        return false;
-    }
+        return 0;
 
-    return true;
+    if (rankDiff == 2)
+        return (rank == 2 || rank == 7) && fileDiff == 0;
+
+    if (rankDiff != 1)
+        return 0;
+
+    if (fileDiff == 1)
+        return 2;
+
+    return 1;
 }
--- others/chess-test/src/pieces/piece.cpp
+++ others/chess-test/src/pieces/piece.cpp
@@ -20,11 +20,11 @@ uint8_t Piece::isMovePossible(uint8_t rank, uint8_t file,
 {
     if (rank < 1 || rank > 8 || file < 1 || file > 8)
     {
-        return 1;
+        return 0;
     }
     if (rankTarget < 1 || rankTarget > 8 || fileTarget < 1 || fileTarget > 8)
     {
-        return 1;
+        return 0;
     }
     return 1;
 }
