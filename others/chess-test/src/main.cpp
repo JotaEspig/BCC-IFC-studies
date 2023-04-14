8d888a75239b647ed613cbdb9af6eed5cbb9c1c9
João Vitor Espig <jotinha1300@gmail.com>
2023-04-13 23:51:20 -0300

    added simple test for pawn movement en passant not working for now

--- others/chess-test/src/main.cpp
+++ others/chess-test/src/main.cpp
@@ -9,14 +9,30 @@
 int main()
 {
     auto b = new chess::Board();
-    auto move = chess::Move(chess::Pieces::WhitePawn, 7, 4, 5, 4);
-    b->doMove(move);
-    std::cout << b << "\n";
-    //chess::Piece *piece;
-    //chess::Pawn pawn = chess::Pawn(true);
-    //piece = &pawn;
+    auto move = chess::Move(chess::Pieces::WhitePawn, 4, 7, 4, 5);
+    auto result = b->doMove(move); // 0
 
-    //std::cout << piece->isMovePossible(2, 4, 4, 4);
+    move = chess::Move(chess::Pieces::WhitePawn, 4, 2, 4, 5);
+    result = b->doMove(move); // 0
+
+    move = chess::Move(chess::Pieces::WhitePawn, 4, 2, 3, 4);
+    result = b->doMove(move); // 0
+
+    move = chess::Move(chess::Pieces::WhitePawn, 4, 2, 5, 4);
+    result = b->doMove(move); // 0
+
+    move = chess::Move(chess::Pieces::WhitePawn, 4, 2, 4, 4);
+    result = b->doMove(move); // 1
+
+    move = chess::Move(chess::Pieces::WhitePawn, 4, 2, 5, 3);
+    result = b->doMove(move); // 2
+
+    std::cout << b << "\n";
+    // chess::Piece *piece;
+    // chess::Pawn pawn = chess::Pawn(true);
+    // piece = &pawn;
+
+    // std::cout << piece->isMovePossible(2, 4, 4, 4);
 
     return 0;
 }
