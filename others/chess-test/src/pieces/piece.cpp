e8e7f8b1f6d1d4623119dd07c86bb4abc06a8c4c
João Vitor Espig <jotinha1300@gmail.com>
2023-04-16 01:45:16 -0300

    fixed Piece::IsMovePossible

--- others/chess-test/src/pieces/piece.cpp
+++ others/chess-test/src/pieces/piece.cpp
@@ -16,16 +16,15 @@ bool Piece::getIsWhite()
     return this->isWhite;
 }
 
-ResultCode Piece::isMovePossible(uint8_t rank, uint8_t file,
-                                 uint8_t rankTarget, uint8_t fileTarget)
+ResultCode Piece::isMovePossible(uint8_t file, uint8_t rank,
+                                 uint8_t fileTarget, uint8_t rankTarget)
 {
-    if (rank < 1 || rank > 8 || file < 1 || file > 8)
-    {
+    if (file == fileTarget && rank == rankTarget)
         return ResultCode::Invalid;
-    }
-    if (rankTarget < 1 || rankTarget > 8 || fileTarget < 1 || fileTarget > 8)
-    {
+    if (file < 1 || file > 8 || rank < 1 || rank > 8)
         return ResultCode::Invalid;
-    }
+    if (fileTarget < 1 || fileTarget > 8 || rankTarget < 1 || rankTarget > 8)
+        return ResultCode::Invalid;
+
     return ResultCode::Normal;
 }
