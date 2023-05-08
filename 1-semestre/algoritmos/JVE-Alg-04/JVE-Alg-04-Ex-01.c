f1d6442956098baa8aeb9076f52d489581059a32
João Vitor Espig <jotinha1300@gmail.com>
2023-05-08 19:56:56 -0300

    added exercise 1

---
+++ 1-semestre/algoritmos/JVE-Alg-04/JVE-Alg-04-Ex-01.c
@@ -0,0 +1,34 @@
+/*
+Média aritmética. Escreva um programa Python que calcula a média aritmética de um
+conjunto de valores fornecidos pelo usuário. O usuário deve entrar com o valor 0 indicando
+que não serão mais fornecidos novos valores. Seu programa deve exibir uma mensagem de
+erro se o primeiro valor fornecido pelo usuário for 0.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "list.h"
+
+int main()
+{
+    num_list_t *nl = new_num_list();
+    float input = 0;
+    do
+    {
+        if (input != 0) num_list_append(nl, input);
+
+        printf("Numero (0 para sair): ");
+        scanf("%f", &input);
+    } while(input != 0);
+
+    double sum = 0;
+    for (size_t i; i < nl->size; i++)
+    {
+        sum += nl->values[i];
+    }
+    double average = sum / nl->size;
+    printf("media: %.1lf\n", average);
+
+    return 0;
+}
