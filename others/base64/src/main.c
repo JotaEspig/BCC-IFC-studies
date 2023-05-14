df5030873c04dce20d683532829ce0666ae49561
João Vitor Espig <jotaespig@gmail.com>
2023-05-14 19:34:48 -0300

    improved printf to show encoded

--- others/base64/src/main.c
+++ others/base64/src/main.c
@@ -7,11 +7,10 @@ int main(int argc, char *argv[])
 {
     char *input = NULL;
     size_t input_size = 0;
-    ssize_t input_len = getline(&input, &input_size, stdin);
-    input[input_len - 1] = 0;
+    getline(&input, &input_size, stdin);
 
     char *encoded = base64_encode(input);
-    printf("%s", encoded);
+    printf("%s\n", encoded);
 
     free(input);
     free(encoded);
