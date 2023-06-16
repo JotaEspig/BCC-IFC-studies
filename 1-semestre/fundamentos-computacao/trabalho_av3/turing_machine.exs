883ccac2b110760158ce5351776206e36cacfeb0
João Vitor Espig <jotinha1300@gmail.com>
2023-06-16 13:02:12 -0300

    renamed file

--- 1-semestre/fundamentos-computacao/trabalho_av3/turing_machine.exs
+++ 1-semestre/fundamentos-computacao/trabalho_av3/turing_machine.exs
@@ -8,22 +8,21 @@ defmodule TuringMachine do
 
   defp delta(state, symbol) do
     case {state, symbol} do
-      {:right, " "} ->
-        {:carry, " ", :L}
-      {:right, _} ->
-        {:right, symbol, :R}
-      {:carry, "1"} ->
-        {:carry, "0", :L}
-      {:carry, _} ->
-        {:done, "1", :L}
+      {:right, @blank_symbol} -> {:carry, @blank_symbol, :L}
+      {:right, "1"} -> {:right, "1", :R}
+      {:right, "0"} -> {:right, "0", :R}
+      {:carry, @blank_symbol} -> {:done, "1", :L}
+      {:carry, "1"} -> {:carry, "0", :L}
+      {:carry, "0"} -> {:done, "1", :L}
+
       _ ->
-        raise ArgumentError, message: "Valores inválidos"
+        raise ArgumentError, message: "Valores inválidos (#{state}, \"#{symbol}\")"
     end
   end
 
   defp run_recursive(tape, idx, state) do
     if state in @final_states do
-      String.replace(tape, " ", "")
+      String.replace(tape, @blank_symbol, "")
     else
       {state, symbol, direction} =
         String.at(tape, idx)
@@ -36,11 +35,12 @@ defmodule TuringMachine do
         |> List.insert_at(idx, symbol)
         |> to_string()
 
-      idx = if direction == :R do
-        idx + 1
-      else
-        idx - 1
-      end
+      idx =
+        if direction == :R do
+          idx + 1
+        else
+          idx - 1
+        end
 
       run_recursive(tape, idx, state)
     end
@@ -48,8 +48,8 @@ defmodule TuringMachine do
 
   @spec run(String.t) :: String.t
   def run(tape) do
-    tape = tape <> " "
+    tape = @blank_symbol <> tape <> @blank_symbol
     q0 = @initial_state
-    run_recursive(tape, 0, q0)
+    run_recursive(tape, 1, q0)
   end
 end
