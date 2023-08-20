aacec631d485454839bebe80958ee98489c80265
João Vitor Espig <jotaespig@gmail.com>
2023-08-20 14:42:19 -0300

    added warning for run.sh

--- others/os-test/my-os/run.sh
+++ others/os-test/my-os/run.sh
@@ -1,3 +1,8 @@
+# deprecated file. Use: make
+
+echo "=== DEPRECATED ==="
+echo -e "USE: make\n\n"
+
 i386-elf-gcc -ffreestanding -m32 -g -c "src/kernel.c" -o "obj/kernel.o"
 nasm "src/kernel_entry.asm" -f elf -o "obj/kernel_entry.o"
 i386-elf-ld -o "bin/full_kernel.bin" -Ttext 0x1000 "obj/kernel_entry.o" "obj/kernel.o" --oformat binary
