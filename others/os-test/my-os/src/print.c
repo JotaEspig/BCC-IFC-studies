116921c846e86769818d7c116342fc177ae7440c
João Vitor Espig <jotaespig@gmail.com>
2023-08-20 15:36:36 -0300

    print changed

--- others/os-test/my-os/src/print.c
+++ others/os-test/my-os/src/print.c
@@ -1,12 +1,13 @@
 #include <print.h>
 
+unsigned short int text_color = 0x0F;
+
 void print(char *str)
 {
     char *screen_ptr = (char *)0xb8000;
     while (*str != 0)
     {
-        *screen_ptr = *str;
-        screen_ptr += 2;
-        str++;
+        *screen_ptr++ = *str++;
+        *screen_ptr++ = text_color;
     }
 }
