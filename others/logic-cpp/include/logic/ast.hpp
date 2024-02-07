7efe8905f9729abd5c5a52be50b465b698214322
João Vitor Espig <jotaespig@gmail.com>
2024-02-07 12:14:26 -0300

    docs(ast): add docstring for doxygen

--- others/logic-cpp/include/logic/ast.hpp
+++ others/logic-cpp/include/logic/ast.hpp
@@ -13,20 +13,55 @@
 namespace logic
 {
 
+/**
+ * \brief Represents an AST of Tokens
+ * \author João Vitor Espig (JotaEspig)
+ * \date 07 February, 2024
+ * \version 07 February, 2024
+ **/
 struct AST
 {
+    /**
+     * \brief Represents a node of the AST of Tokens
+     * \author João Vitor Espig (JotaEspig)
+     * \date 07 February, 2024
+     * \version 07 February, 2024
+     **/
     struct Node
     {
+        /** Node value **/
         Token value;
+        /** Left Node **/
         std::unique_ptr<Node> left;
+        /** Right Node **/
         std::unique_ptr<Node> right;
 
+        /**
+         * \brief Node constructor
+         * \author João Vitor Espig (JotaEspig)
+         * \date 07 February, 2024
+         * \version 07 February, 2024
+         **/
         Node();
+        /**
+         * \brief Node constructor
+         * \author João Vitor Espig (JotaEspig)
+         * \date 07 February, 2024
+         * \version 07 February, 2024
+         * \param token - const reference to token
+         **/
         Node(const Token &token);
     };
 
+    /** Root of the tree **/
     std::unique_ptr<Node> root;
 
+    /**
+     * \brief AST constructor
+     * \author João Vitor Espig (JotaEspig)
+     * \date 07 February, 2024
+     * \version 07 February, 2024
+     **/
     AST();
 };
 
