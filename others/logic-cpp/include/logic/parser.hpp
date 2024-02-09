8daf97d795bee79d78d9cc67da9fd98f539b4950
João Vitor Espig <jotaespig@gmail.com>
2024-02-09 13:59:41 -0300

    docs(parser): add docstring for doxygen

--- others/logic-cpp/include/logic/parser.hpp
+++ others/logic-cpp/include/logic/parser.hpp
@@ -15,31 +15,116 @@
 namespace logic
 {
 
+/**
+ * \brief Parser
+ * \author João Vitor Espig (JotaEspig)
+ * \date 09 February, 2024
+ * \version 09 February, 2024
+ **/
 class Parser
 {
     typedef std::vector<Token> token_vec;
 
 public:
+    /** Error occured during parsing **/
     std::string error;
 
+    /**
+     * \brief Default Lexer constructor
+     * \author João Vitor Espig (JotaEspig)
+     * \date 04 February, 2024
+     * \version 04 February, 2024
+     **/
     Parser();
+    /**
+     * \brief Default Lexer constructor
+     * \author João Vitor Espig (JotaEspig)
+     * \date 04 February, 2024
+     * \version 04 February, 2024
+     * \param tokens - token_vec reference
+     **/
     Parser(const token_vec &tokens);
 
+    /**
+     * \brief member tokens get
+     * \author João Vitor Espig (JotaEspig)
+     * \date 09 February, 2024
+     * \version 09 February, 2024
+     **/
     const token_vec &tokens() const;
+    /**
+     * \brief member begin get
+     * \author João Vitor Espig (JotaEspig)
+     * \date 09 February, 2024
+     * \version 09 February, 2024
+     **/
     const token_vec::iterator &begin() const;
+    /**
+     * \brief member end get
+     * \author João Vitor Espig (JotaEspig)
+     * \date 09 February, 2024
+     * \version 09 February, 2024
+     **/
     const token_vec::iterator &end() const;
+    /**
+     * \brief member curr get
+     * \author João Vitor Espig (JotaEspig)
+     * \date 09 February, 2024
+     * \version 09 February, 2024
+     **/
     const token_vec::iterator &curr() const;
+    /**
+     * \brief set error as msg and returns nullptr
+     * \author João Vitor Espig (JotaEspig)
+     * \date 09 February, 2024
+     * \version 09 February, 2024
+     * \param msg - error message
+     **/
     AST::node_ptr set_error(std::string msg);
+    /**
+     * \brief increments the curr iterator
+     * \author João Vitor Espig (JotaEspig)
+     * \date 09 February, 2024
+     * \version 09 February, 2024
+     **/
     void next();
+    /**
+     * \brief generates the AST according to the token_vec tokens
+     * \author João Vitor Espig (JotaEspig)
+     * \date 09 February, 2024
+     * \version 09 February, 2024
+     **/
     AST generate_ast();
+    /**
+     * \brief parses an expression
+     * \author João Vitor Espig (JotaEspig)
+     * \date 09 February, 2024
+     * \version 09 February, 2024
+     **/
     AST::node_ptr parse_expression();
+    /**
+     * \brief parses a term
+     * \author João Vitor Espig (JotaEspig)
+     * \date 09 February, 2024
+     * \version 09 February, 2024
+     **/
     AST::node_ptr parse_term();
+    /**
+     * \brief parses a factor
+     * \author João Vitor Espig (JotaEspig)
+     * \date 09 February, 2024
+     * \version 09 February, 2024
+     **/
     AST::node_ptr parse_factor();
 
 private:
+    /** tokens vector **/
     token_vec _tokens;
+    /**  begin iterator **/
     token_vec::iterator _begin;
+    /**  end iterator **/
     token_vec::iterator _end;
+    /** curr iterator **/
     token_vec::iterator _curr;
 };
 
