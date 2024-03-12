ace5af4b8d6961146a8091ee866dd7c059acd9b0
João Vitor Espig <jotaespig@gmail.com>
2024-03-12 00:55:18 -0300

    add determinant() and multiplyByScalar()

--- 3-semestre/algebra-linear/matrix-calculator/main.go
+++ 3-semestre/algebra-linear/matrix-calculator/main.go
@@ -4,10 +4,13 @@ import "fmt"
 
 func main() {
 	fmt.Println("Matrix calculator")
-	m1 := New([][]float32{{0.5, 0.2, 1}, {-2, 3, 7}})
-	m2 := New([][]float32{{4, -4}, {10, -5}, {-1, -2}})
+	m1, _ := New([][]float32{{0.5, 0.2, 1}, {-2, 3, 7}})
+	m2, _ := New([][]float32{{4, -4}, {10, -5}, {-1, -2}})
 
 	m1.Print()
 	m2.Print()
-	m1.Multiply(m2).Print()
+
+	m3, _ := m1.Multiply(m2)
+	m3.Print()
+	fmt.Println(m3.Determinant())
 }
--- 3-semestre/algebra-linear/matrix-calculator/matrix.go
+++ 3-semestre/algebra-linear/matrix-calculator/matrix.go
@@ -3,10 +3,11 @@ package main
 import (
 	"errors"
 	"fmt"
+	"math"
 )
 
 type number interface {
-	uint8 | uint16 | uint32 | uint64 | uint | int8 | int16 | int32 | int64 | int | float32 | float64
+	int8 | int16 | int32 | int64 | int | float32 | float64
 }
 
 type Matrix[T number] struct {
@@ -26,24 +27,38 @@ func NewWithoutData[T number](r uint16, c uint16) *Matrix[T] {
 	return result
 }
 
-func New[T number](data [][]T) *Matrix[T] {
+func New[T number](data [][]T) (*Matrix[T], error) {
 	r := uint16(len(data))
 	if r == 0 {
-		panic(errors.New("rows amound cannot be zero"))
+		return nil, errors.New("rows amound cannot be zero")
 	}
 
 	c := uint16(len(data[0]))
 	if c == 0 {
-		panic(errors.New("rows amound cannot be zero"))
+		return nil, errors.New("rows amound cannot be zero")
 	}
 
-	return &Matrix[T]{r, c, data}
+	return &Matrix[T]{r, c, data}, nil
 }
 
-func (m *Matrix[T]) Multiply(m2 *Matrix[T]) *Matrix[T] {
+func (m *Matrix[T]) MultiplyByScalar(num T) (*Matrix[T], error) {
+	result, err := New(m.data)
+	if err != nil {
+		return nil, err
+	}
+
+	for i := range m.rows {
+		for j := range m.columns {
+			result.data[i][j] *= num
+		}
+	}
+	return result, nil
+}
+
+func (m *Matrix[T]) Multiply(m2 *Matrix[T]) (*Matrix[T], error) {
 	if m.columns != m2.rows {
 		errorMsg := fmt.Sprintf("Cannot multiply matrix: m.c == %d and m2.r == %d", m.columns, m2.rows)
-		panic(errors.New(errorMsg))
+		return nil, errors.New(errorMsg)
 	}
 
 	result := NewWithoutData[T](m.rows, m2.columns)
@@ -55,9 +70,81 @@ func (m *Matrix[T]) Multiply(m2 *Matrix[T]) *Matrix[T] {
 		}
 	}
 
-	return result
+	return result, nil
+}
+
+func (m *Matrix[T]) Determinant() (T, error) {
+	if m.rows-m.columns != 0 {
+		return 0, errors.New("You cannot get a determinant from non-square matrix")
+	}
+	if m.rows == 1 && m.columns == 1 {
+		return m.data[0][0], nil
+	}
+
+	var result T
+	// It uses Laplace at first row every time
+	row := uint16(0)
+	for j := range m.columns {
+		cofactor, err := m.Cofactor(row, j)
+		if err != nil {
+			return 0, err
+		}
+		det, err := cofactor.Determinant()
+		if err != nil {
+			return 0, err
+		}
+
+		result += m.data[row][j] * T(math.Pow(-1, float64(j))) * det
+	}
+	return result, nil
+}
+
+func (m *Matrix[T]) Cofactor(r, c uint16) (*Matrix[T], error) {
+	if r >= m.rows || c >= m.columns {
+		return nil, errors.New("Invalid cofactor matrix")
+	}
+
+	result := NewWithoutData[T](m.rows-1, m.columns-1)
+	if result.rows == 0 || result.columns == 0 {
+		return nil, errors.New("Invalid cofactor matrix")
+	}
+
+	var count1, count2 int
+	for i := range m.rows {
+		if i == r {
+			continue
+		}
+
+		for j := range m.columns {
+			if j == c {
+				continue
+			}
+
+			result.data[count1][count2] = m.data[i][j]
+			count2++
+		}
+
+		count1++
+	}
+
+	return result, nil
+}
+
+func (m *Matrix[T]) Inverse() (*Matrix[T], error) {
+	if det, _ := m.Determinant(); det == 0 {
+		return nil, errors.New("matrix: cannot get inverse, determinant == 0")
+	}
+	return nil, nil
 }
 
 func (m Matrix[T]) Print() {
-	fmt.Println(m.data)
+	if m.rows == 0 {
+		return
+	}
+
+	fmt.Println("[-")
+	for i := range m.rows {
+		fmt.Printf("%v,\n", m.data[i])
+	}
+	fmt.Println("-]")
 }
