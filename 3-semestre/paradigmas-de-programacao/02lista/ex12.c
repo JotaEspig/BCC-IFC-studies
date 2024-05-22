99a48a60e34a593ccb55eaf5692bc5bbe37c689b
João Vitor Espig <jotaespig@gmail.com>
2024-05-22 19:17:05 -0300

    fix cpf validator

--- 3-semestre/paradigmas-de-programacao/02lista/ex12.c
+++ 3-semestre/paradigmas-de-programacao/02lista/ex12.c
@@ -10,6 +10,16 @@ int is_valid_cpf(char *cpf) {
     if (strlen(cpf) != 11) {
         return 0;
     }
+    int valid = 0;
+    for (int i = 0; i < 1; ++i) {
+        if (cpf[i] != cpf[i + 1]) {
+            valid = 1;
+            break;
+        }
+    }
+    if (!valid) {
+        return 0;
+    }
 
     int first_verification_num = cpf[9] - '0';
     int last_verification_num = cpf[10] - '0';
@@ -54,4 +64,6 @@ int main() {
     printf("123.456.789-90: %d\n", is_valid_cpf("12345678990"));
     // cpf gerado pelo site: https://www.4devs.com.br/gerador_de_cpf
     printf("614.115.480-86: %d\n", is_valid_cpf("61411548086"));
+    printf("000.000.000-00: %d\n", is_valid_cpf("00000000000"));
+    printf("111.111.111-11: %d\n", is_valid_cpf("11111111111"));
 }
