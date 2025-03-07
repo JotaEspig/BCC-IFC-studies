4b7603d413adfd8f434d124b4c7763829836f5b0
João Vitor Espig <jotaespig@gmail.com>
2025-03-07 20:56:57 -0300

    :)

--- 5-semetre/estatistica/02_28_exercicios/asa.R
+++ 5-semetre/estatistica/02_28_exercicios/asa.R
@@ -16,8 +16,8 @@ SD <- 19.8
 z <- 1.96
 error_margin <- 5
 # n0 <- (z^2 * (prob) * (1 - (prob))) / (error_margin^2)
-n0 <- (z^2 * sd(data$PESO)^2) / (error_margin^2)
-sample_size <- n0 / (1 + ((n0 - 1) / nrow(data)))
+n0 <- (z^2 * SD^2) / (error_margin^2)
+sample_size <- n0 / (1 + ((n0 / nrow(data)))
 catln("N_0: ", n0)
 catln("Tamanho populacional: ", nrow(data))
 catln("Dimensionamento da amostra: ", sample_size)
